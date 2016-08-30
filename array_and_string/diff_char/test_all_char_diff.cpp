6e6ef6695bccde4e45f53ad3dd02d6a5a3ac50bf
lordstone <chengxis@usc.edu>
2016-08-31 04:16:16 +0800

    init and add arraystring

---
+++ array_and_string/diff_char/test_all_char_diff.cpp
@@ -0,0 +1,45 @@
+/*
+	lordstone:
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+bool isDupChar(string s1, string s2){
+	if(s1 == s2){
+		return s1.size() == 0 ? false : true;
+	}else{
+
+		bool charMap [255];		
+		for(int i = 0; i < s1.size(); ++ i )
+		{
+			charMap[static_cast<int>(s1[i])] = true;
+		}
+		for(int i = 0; i < s2.size(); ++ i){
+			if(charMap[static_cast<int>(s2[i])])
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+}
+
+int main(){
+
+	string v1 = "";
+	string v2 = "avvdfdb";
+
+	cin >> v1;
+
+	cout << ("Your input:" + v1) << endl;
+
+	cout << (isDupChar(v1, v2) ? 
+	"duplicate Char detected" : "No duplicate char..")
+	 << endl;	
+
+
+	return 0;
+}
