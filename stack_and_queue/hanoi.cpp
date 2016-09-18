9cc727bd79c2bd149c4c77bf88caa2d78f6d7e0e
lordstone <chengxis@usc.edu>
2016-09-18 14:44:15 +0800

    finish physical hanoi

--- stack_and_queue/hanoi.cpp
+++ stack_and_queue/hanoi.cpp
@@ -1,37 +1,60 @@
 #include <iostream>
-#include <stack>
+#include <vector>
 #include <string>
 
 using namespace std;
 
 class Hanoi{
 public:
+
 	Hanoi(int input){
 		n_pieces = input > 0 ? input : 3;
+		init();
 	}
 	
 	void result(){
-		// init();
 		HanoiExpress(n_pieces, 0, 1, 2);
 	}
 
 	void HanoiExpress(int n, int from, int buffer, int to){
 		if(n == 1){
 			cout << "Move " << n << " from " << from << " to " << to << " ." << endl;
+			move(from, to);
 		}else{
 			HanoiExpress(n-1, from, to, buffer);
 			cout << "Move " << n << " from " << from << " to " << to << " ." << endl;
+			move(from, to);
 			HanoiExpress(n-1, buffer, from, to);
+		}
+	}
+
+	void display(){
+		for(int j = 0; j < 3; ++j){
+			cout << "Number " << j+1 << " Tower" << endl;
+			cout << "|-" ;
+			for(auto i = mystk[j].begin(); i != mystk[j].end(); ++i){
+				cout << *i << "-";
+			}
+			cout << "|" << endl;
 		}
 	}
 
 private:
 	int n_pieces;
-	stack<int> mystk [3];
+	vector<int> mystk [3];
+
+	void move(int from, int to){
+		if(!mystk[from].empty()){
+			if(mystk[to].empty() or mystk[from].back() < mystk[to].back()){
+				mystk[to].push_back(mystk[from].back());
+				mystk[from].pop_back();
+			}
+		}
+	}
 
 	void init(){
 		for(int i = n_pieces; i > 0; --i){
-			mystk[0].push(i);
+			mystk[0].push_back(i);
 		}
 	}
 };
@@ -47,6 +70,8 @@ int main(int argc, char* argv []){
 		}
 	}	
 	Hanoi h (n);
+	h.display();
 	h.result();
+	h.display();
 	return 0;
 }
